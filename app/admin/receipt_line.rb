46c35dcb412d80bee31f56f10a40f06177feb4e5
Raghu Betina <raghu@firstdraft.com>
2016-10-29 13:03:32 +0000

    Whitelist columns in ReceiptLine for ActiveAdmin

--- app/admin/receipt_line.rb
+++ app/admin/receipt_line.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ReceiptLine do
 
+ permit_params :claimer_id, :description, :line_value, :receipt_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
